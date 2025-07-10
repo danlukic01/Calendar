1efb24988ce9cbf8545100b0b31d80f71b55ed5f
danlukic01 <141298464+danlukic01@users.noreply.github.com>
2025-07-10 12:43:32 +1000

    c

--- Calendar.Api/Program.cs
+++ Calendar.Api/Program.cs
@@ -9,10 +9,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<CalendarConversionService>();
 
-builder.Services.AddDbContextFactory<AppDbContext>(
-    options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")),
-    ServiceLifetime.Scoped // recommended
-);
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
