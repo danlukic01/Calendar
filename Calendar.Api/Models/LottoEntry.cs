b19f209acdfa1d9decc973685ab9705f98d2cf5f
danlukic01 <141298464+danlukic01@users.noreply.github.com>
2025-07-15 11:06:42 +1000

    Add supplement numbers for Ozlotto and Tattslotto

--- Calendar.Api/Models/LottoEntry.cs
+++ Calendar.Api/Models/LottoEntry.cs
@@ -15,5 +15,8 @@ namespace Calendar.Api.Models
         public int Number6 { get; set; }
         public int Number7 { get; set; }
         public int Powerball { get; set; }
+        public int Supplement1 { get; set; }
+        public int Supplement2 { get; set; }
+        public int Supplement3 { get; set; }
     }
 }
